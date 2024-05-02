b4ead46822539ad2d2de8f9f9ce102cbb0170afb
RodriguezJimenezRicardo <rodriguez.jimenez.jose.ricardo027@gmail.com>
2024-05-02 04:30:43 -0600

    divi6, mux y test

--- divi6.vhd
+++ divi6.vhd
@@ -1,58 +1,66 @@
-library ieee;
-use ieee.std_logic_1164.all;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity divi6 is
     port(
         clk: in std_logic;
-        A, B: in std_logic_vector(11 downto 0);
-        result: out std_logic_vector(11 downto 0)
+        A, B: in std_logic_vector(15 downto 0);
+        result: out std_logic_vector(15 downto 0)
     );
 end entity;
 
 architecture a_divi6 of divi6 is
-    component add_sub_12 is
-        port(
-            A, B: in std_logic_vector(11 downto 0);
-            sub: in std_logic;
-            S: inout std_logic_vector(11 downto 0);
-            cout: out std_logic
+    component SumRest16Bits is
+        Port (
+            A : in std_logic_vector(15 downto 0);
+            B : in std_logic_vector(15 downto 0);
+            Cin : in std_logic;
+            Op : in std_logic;
+            Res : out std_logic_vector(15 downto 0);
+            Cout : out std_logic
         );
     end component;
 
-    signal subtractor_input, one: std_logic_vector(11 downto 0);
-    signal subtractor_output, increment_output: std_logic_vector(11 downto 0);
-    signal subtraction_counter: std_logic_vector(11 downto 0) := (others => '0');
-    signal operation_complete: std_logic := '0';
-    
-begin
-    one <= "000000000001";
+    signal B_mod : std_logic_vector(15 downto 0);
+    signal intermediate_carry : std_logic_vector(16 downto 0);
+    signal Quotient : std_logic_vector(15 downto 0) := (others => '0');
+    signal Remainder : std_logic_vector(15 downto 0) := (others => '0');
+    signal Temp_result : std_logic_vector(15 downto 0) := (others => '0'); -- Inicializar Temp_result
+    signal Count : std_logic_vector(15 downto 0) := (others => '0');
+    signal unused_signal : std_logic;
 
+begin
+    -- Component instances
+    Resta: SumRest16Bits port map (Remainder, B_mod, intermediate_carry(0), '1', Temp_result, unused_signal);
+    Suma : SumRest16Bits port map (Count, "0000000000000001", '0', '0', Count, unused_signal); -- Utilizando Suma como contador
+
+    -- Process for division logic
     process(clk)
     begin
-        if (clk'event and clk='1') then
-		    if B = "000000000000" then
-				result <= "111111111111";
-				operation_complete <= '1'; 
-            elsif operation_complete = '0' then
-                if subtractor_input < B then
-                    result <= subtraction_counter;
-                    operation_complete <= '1';
-                else
-                    subtractor_input <= subtractor_output;
-                    subtraction_counter <= increment_output;
-                end if;
+        if rising_edge(clk) then
+            -- Inicialización de la división
+            if Remainder = "0000000000000000" then
+                Remainder <= A;
+            end if;
+
+            -- Comprobación si la división ha finalizado
+            if Remainder >= B then
+                -- Realizar la división
+                B_mod <=  B;
+                intermediate_carry(0) <= '1';
+                Remainder <= Temp_result;
+            else
+                -- No se puede dividir más, finalizar proceso
+                Remainder <= Remainder;
+                B_mod <= B;
+                intermediate_carry(0) <= '0';
+                Quotient <= Count;
             end if;
         end if;
     end process;
-    
-    subtraction_instance: add_sub_12 port map(subtractor_input,B,'1',subtractor_output,open);
-    increment_instance: add_sub_12 port map(subtraction_counter,one,'0',increment_output,open);
-    
-    initial_process: process(A, B)
-    begin
-        subtractor_input <= A;
-        subtraction_counter <= "000000000000";
-        operation_complete <= '0';
-    end process;
+
+    -- Asignar el valor del contador como salida result
+    result <= Quotient;
 
 end architecture;
